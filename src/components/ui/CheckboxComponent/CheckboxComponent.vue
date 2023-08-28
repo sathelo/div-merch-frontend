99c0f94f338cebd5ad1bae4b21b25fcc17fe0774
sathelo <sashenkocasha@mail.ru>
2023-08-28 11:04:19 +0300

    fix: Поправил стилизацию для веб-компонента checkbox

--- src/components/ui/CheckboxComponent/CheckboxComponent.vue
+++ src/components/ui/CheckboxComponent/CheckboxComponent.vue
@@ -56,82 +56,73 @@ const { uniqueId } = useUniqueId();
     border: 1px solid @grey-gradation--100;
     border-radius: 4px;
     background: @grey-gradation--100;
-    transition: 0.2s;
     margin-right: 8px;
     cursor: pointer;
-
-    display: inline-block;
-    flex-shrink: 0;
-    flex-grow: 0;
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 50% 50%;
   }
 
   /* стили при наведении курсора */
-  &__input:not(:disabled):not(:checked) + &__label:hover::before {
-    background: @grey-gradation--300;
-  }
+  &__input:not(:disabled):not(:checked) + &__label:hover {
+    & {
+      .text-s;
+      color: @grey-gradation--300;
+    }
 
-  &__label:hover {
-    .text-s;
-    color: @grey-gradation--300;
+    &::before {
+      background: @grey-gradation--300;
+    }
   }
 
   /* стили для активного */
-  &__input:not(:disabled):active + &__label::before {
-    background: @grey-gradation--black;
-  }
+  &__input:not(:disabled):active + &__label {
+    & {
+      .text-s;
+      color: @grey-gradation--black;
+    }
 
-  &__label:active {
-    .text-s;
-    color: @grey-gradation--black;
+    &::before {
+      background: @grey-gradation--black;
+    }
   }
 
   /* стили для фокуса */
-  &__input:focus + &__label::before {
-    background: @grey-gradation--300;
-  }
+  &__input:focus + &__label {
+    & {
+      .text-s;
+      color: @grey-gradation--300;
+    }
 
-  &__label:focus {
-    .text-s;
-    color: @grey-gradation--300;
-  }
-
-  /* стили для находящегося в фокусе и не checked */
-  &__input:focus:not(:checked) + &__label::before {
-    border-color: #80bdff;
+    &::before {
+      background: @grey-gradation--300;
+    }
   }
 
   /* стили для checked */
-  &__input:checked + &__label::before {
-    border-color: #0b76ef;
-    background-color: #0b76ef;
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+  &__input:checked + &__label {
+    & {
+      .text-s;
+      color: @grey-gradation--black;
+    }
+
+    &::before {
+      .flex-properties(flex, center, center);
+      background: @grey-gradation--black;
+      background-image: url("/icons/check.svg");
+      background-repeat: no-repeat;
+      background-position: center;
+    }
   }
 
   /* стили для disabled */
-  &__input:disabled + &__label::before {
-    background-color: #e9ecef;
+  &__input:disabled + &__label {
+    & {
+      .text-s;
+      color: @grey-gradation--200;
+    }
+
+    &::before {
+      border: 1px solid @grey-gradation--100;
+      background: @grey-gradation--white;
+    }
   }
-
-  // &:focus {
-  //   .text-s;
-  //   color: @grey-gradation--300;
-
-  //   .checkbox__check {
-  //     background: @grey-gradation--300;
-  //   }
-  // }
-
-  // &:disabled {
-  //   .text-s;
-  //   color: @grey-gradation--200;
-
-  //   .checkbox__check {
-  //     border: 1px solid @grey-gradation--100;
-  //     background: @grey-gradation--white;
-  //   }
-  // }
 }
 </style>

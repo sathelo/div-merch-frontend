e31e558f4d4a34b49ddea6eb7087af3173ab52e5
sathelo <sathelo@yandex.ru>
2023-11-08 13:19:33 +0300

    feat: Добавил масштабирование (plugin postcss-pxtorem)

---
+++ postcss.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    plugins: {
+      'postcss-pxtorem': {
+        rootValue: 16, // Базовый размер шрифта в пикселях
+        unitPrecision: 5, // Точность округления до 5 знаков после запятой
+        propList: [
+          '*',
+          '!border-left',
+          '!border-right',
+          '!border-top',
+          '!border-bottom',
+          '!border',
+          '!outline',
+        ], // Конвертировать все свойства, содержащие значения в vw
+        selectorBlackList: [], // Исключить селекторы из конвертации
+        replace: true, // Заменять исходные значения свойств
+        mediaQuery: false, // Разрешить конвертацию внутри медиа-запросов
+        minPixelValue: 0 // Минимальное значение в пикселях, при котором будет производиться конвертация
+      }
+    }
+  }
+  
